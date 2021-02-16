675c4725d144a9d8886d88b8a6127e7f0578fe75
akyhcs <52922974+akyhcs@users.noreply.github.com>
2021-02-16 18:39:07 +0530

    Update AirlineApplication.java

--- airline/src/main/java/com/mindtree/demo/AirlineApplication.java
+++ airline/src/main/java/com/mindtree/demo/AirlineApplication.java
@@ -2,11 +2,16 @@ package com.mindtree.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+org.springframework.core.env.Environment
 
 @SpringBootApplication
 public class AirlineApplication {
 
+	@Autowired
+	private Environment env;
+
 	public static void main(String[] args) {
+	        System.out.println(env.getProperty("mysql_service"));
 		SpringApplication.run(AirlineApplication.class, args);
 	}
 
