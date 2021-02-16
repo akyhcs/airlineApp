589375dd87d300677834584a6fda285a3b516c88
akyhcs <52922974+akyhcs@users.noreply.github.com>
2021-02-16 18:47:04 +0530

    Update AirlineApplication.java

--- airline/src/main/java/com/mindtree/demo/AirlineApplication.java
+++ airline/src/main/java/com/mindtree/demo/AirlineApplication.java
@@ -3,6 +3,7 @@ package com.mindtree.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.core.env.Environment;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @SpringBootApplication
 public class AirlineApplication {
